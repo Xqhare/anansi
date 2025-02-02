fca5650b47815ad5ed74bd2dba78779789ddcced
Xqhare <xqhare@web.de>
2025-02-02 17:07:09 +0100

    filestructure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 mod list;
 mod task;
 mod util;
+
+pub use list::*;
+pub use task::*;
+pub use util::*;
---
+++ src/list/mod.rs
@@ -0,0 +1,6 @@
+use crate::Task;
+
+pub struct List {
+    open_tasks: Vec<Task>,
+    done_tasks: Vec<Task>,
+}
