451243ae15d36dd54fb94e9b19e93b462911abb9
Xqhare <xqhare@web.de>
2025-02-03 19:24:44 +0100

    add: serialising a list into valid todo.txt for writing state to disk

--- src/list/builder.rs
+++ src/list/builder.rs
@@ -12,7 +12,7 @@ pub fn build_default_list<P: Into<PathBuf>>(path: P) -> List {
     }
 }
 
-pub fn deserialize_list<P: Into<PathBuf>, S: AsRef<str>>(path: P, contents: S) -> List {
+pub fn deserialise_list<P: Into<PathBuf>, S: AsRef<str>>(path: P, contents: S) -> List {
     let mut open_tasks: Vec<Task> = Vec::new();
     let mut done_tasks: Vec<Task> = Vec::new();
 
@@ -30,4 +30,17 @@ pub fn deserialize_list<P: Into<PathBuf>, S: AsRef<str>>(path: P, contents: S) -
         open_tasks,
         done_tasks,
     }
+}
+
+pub fn serialise_list(list: &List) -> String {
+    let mut output = String::new();
+    for task in &list.open_tasks {
+        output.push_str(&task.original());
+        output.push('\n');
+    }
+    for task in &list.done_tasks {
+        output.push_str(&task.original());
+        output.push('\n');
+    }
+    output
 }
