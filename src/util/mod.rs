008aa39622d9d1ee18fb30c0a6a0e175d2e887ce
Xqhare <xqhare@web.de>
2025-02-08 00:18:26 +0100

    add: check for month being smaller 13, and day 32

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,6 @@
 mod date;
 mod task_list;
+mod test;
 
 pub use date::*;
 pub use task_list::*;
@@ -11,7 +12,11 @@ pub fn deserialise_date<S: AsRef<str>>(input: S) -> Date {
         let month = split_check[1].parse::<u8>();
         let day = split_check[2].parse::<u8>();
         if year.is_ok() && month.is_ok() && day.is_ok() {
-            Date::new(year.unwrap(), month.unwrap(), day.unwrap())
+            if month.as_ref().unwrap() < &13 && day.as_ref().unwrap() < &32 {
+                Date::new(year.unwrap(), month.unwrap(), day.unwrap())
+            } else {
+                Date::default()
+            }
         } else {
             Date::default()
         }
