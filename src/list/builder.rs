fc5f7bfff029fc7e51394187df5d31b4e03aba3b
Xqhare <xqhare@web.de>
2025-02-21 18:15:56 +0100

    refactor(ListBuilder): serde operations
    Serde of refactored List struct

--- src/list/builder.rs
+++ src/list/builder.rs
@@ -1,32 +1,34 @@
 use std::path::PathBuf;
 
-use crate::Task;
-
-use super::List;
+use super::{TaskID, List, Task};
 
 pub fn build_default_list<P: Into<PathBuf>>(path: P) -> List {
     List {
         file_path: path.into(),
+        tasks: Vec::new(),
         open_tasks: Vec::new(),
         done_tasks: Vec::new(),
     }
 }
 
 pub fn deserialise_list<P: Into<PathBuf>, S: AsRef<str>>(path: P, contents: S) -> List {
-    let mut open_tasks: Vec<Task> = Vec::new();
-    let mut done_tasks: Vec<Task> = Vec::new();
+    let mut tasks: Vec<Task> = Vec::new();
+    let mut open_tasks: Vec<TaskID> = Vec::new();
+    let mut done_tasks: Vec<TaskID> = Vec::new();
 
     for (id, task) in contents.as_ref().lines().enumerate() {
         let task = Task::new(task, id);
         if task.is_done() {
-            done_tasks.push(task);
+            done_tasks.push(id);
         } else {
-            open_tasks.push(task);
+            open_tasks.push(id);
         }
+        tasks.push(task);
     }
 
     List {
         file_path: path.into(),
+        tasks,
         open_tasks,
         done_tasks,
     }
@@ -34,11 +36,7 @@ pub fn deserialise_list<P: Into<PathBuf>, S: AsRef<str>>(path: P, contents: S) -
 
 pub fn serialise_list(list: &List) -> String {
     let mut output = String::new();
-    for task in &list.open_tasks {
-        output.push_str(&task.original());
-        output.push('\n');
-    }
-    for task in &list.done_tasks {
+    for task in &list.tasks {
         output.push_str(&task.original());
         output.push('\n');
     }
