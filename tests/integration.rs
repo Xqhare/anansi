f9fa2fbcc635f631a66667d3013cd694dc3a67f0
Xqhare <xqhare@web.de>
2026-05-11 22:43:56 +0200

    test

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,4 +1,4 @@
-use anansi::*;
+use anansi::{vec::sort_vec_task, *};
 
 #[test]
 fn simple_use() {
@@ -53,4 +53,31 @@ fn simple_extensive_use() {
     let new_task1 = Task::new("Task 1", id);
     list.push_task(new_task1.clone());
     assert_eq!(&new_task1, list.get(id).unwrap());
+}
+
+#[test]
+fn sort_vec() {
+    let mut list = List::new("list.txt");
+    list.add("Task 1");
+    list.add("x Task 2");
+    list.add("Task 3");
+    list.add("(A) Task 8");
+    list.add("(B) Task 9");
+    list.add("(Z) Task 10");
+    list.add("(A) Task 11");
+    list.add("(B) Task 12");
+
+    let mut to_sort = list.tasks();
+    let sort_by = SortBy::Priority;
+
+    sort_vec_task(&mut to_sort, sort_by);
+
+    assert_eq!(to_sort[0].to_string(), "(A) Task 8");
+    assert_eq!(to_sort[1].to_string(), "(A) Task 11");
+    assert_eq!(to_sort[2].to_string(), "(B) Task 9");
+    assert_eq!(to_sort[3].to_string(), "(B) Task 12");
+    assert_eq!(to_sort[4].to_string(), "(Z) Task 10");
+    assert_eq!(to_sort[5].to_string(), "Task 1");
+    assert_eq!(to_sort[6].to_string(), "x Task 2");
+    assert_eq!(to_sort[7].to_string(), "Task 3");
 }
